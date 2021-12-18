af84597e6eb40b7ee43921fd606fa3c6ccd625df
TokarF <tokarferenc89@gmail.com>
2021-12-18 14:19:09 +0100

    Add project files.

---
+++ Gokart Laptime/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Gokart_Laptime.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
